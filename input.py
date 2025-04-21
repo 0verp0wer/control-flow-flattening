5c9772833e06c58b2122e4264eed97801549f456
overpower <69311807+0verp0wer@users.noreply.github.com>
2025-04-21 23:23:23 +0200

    Create input.py

---
+++ input.py
@@ -0,0 +1,12 @@
+def say_hi(name):
+    print("Hello", name)
+
+def add_numbers(a, b):
+    print(a + b)
+
+name = input("insert your name:")
+say_hi(name)
+
+n1 = int(input("insert the first number:"))
+n2 = int(input("insert the second number:"))
+add_numbers(n1, n2)
