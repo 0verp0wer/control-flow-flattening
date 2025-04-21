1e907003685718b8c10ffa445abdb2f406c13330
overpower <69311807+0verp0wer@users.noreply.github.com>
2025-04-21 23:23:42 +0200

    Create out.py

---
+++ out.py
@@ -0,0 +1,88 @@
+Cyron__C4gv = 336
+Cyron__v876 = {336: 420, 436: 369, 486: 452}
+Cyron__dvkj = 0
+Cyron__G554 = 0
+while Cyron__C4gv != 441:
+    match Cyron__C4gv:
+        case 420:
+            Cyron__C4gv += 4
+        case 488:
+            n1 = int(input('insert the first number:'))
+            Cyron__C4gv -= 17
+        case 490:
+            Cyron__C4gv -= 14
+        case 369:
+            Cyron__dvkj = 1
+            Cyron__G554 = 22
+            Cyron__C4gv = 410
+        case 419:
+            add_numbers(n1, n2)
+            Cyron__C4gv += 22
+        case 477:
+            Cyron__dvkj = 25
+            Cyron__G554 = 2
+            Cyron__C4gv = 400
+        case 424:
+            Cyron__C4gv += 12
+        case 436:
+
+            def add_numbers(a, b):
+                Cyron__iUaA = 323
+                Cyron__v4529 = {}
+                Cyron__5KBR = 0
+                Cyron__v2589 = 0
+                while Cyron__iUaA != 430:
+                    match Cyron__iUaA:
+                        case 455:
+                            print(a + b)
+                            Cyron__iUaA -= 25
+                        case 323:
+                            Cyron__iUaA = 455
+                        case _:
+                            Cyron__v4529[str(Cyron__iUaA)] = Cyron__iUaA + Cyron__5KBR
+                            Cyron__iUaA -= Cyron__v2589
+            Cyron__C4gv = Cyron__v876[Cyron__C4gv]
+        case 336:
+
+            def say_hi(name):
+                Cyron__v5906 = 454
+                Cyron__v2643 = {454: 343, 411: 427}
+                Cyron__VgLP = 0
+                Cyron__v6012 = 0
+                while Cyron__v5906 != 427:
+                    match Cyron__v5906:
+                        case 343:
+                            Cyron__v5906 = 411
+                        case 411:
+                            print('Hello', name)
+                            Cyron__v5906 = Cyron__v2643[Cyron__v5906]
+                        case 454:
+                            Cyron__v5906 = Cyron__v2643[Cyron__v5906]
+                        case _:
+                            Cyron__v2643[str(Cyron__v5906)] = Cyron__v5906 + Cyron__VgLP
+                            Cyron__v5906 -= Cyron__v6012
+            Cyron__C4gv = Cyron__v876[Cyron__C4gv]
+        case 486:
+            n2 = int(input('insert the second number:'))
+            Cyron__C4gv = Cyron__v876[Cyron__C4gv]
+        case 452:
+            Cyron__C4gv += 25
+        case 309:
+            name = input('insert your name:')
+            Cyron__C4gv = 490
+        case 388:
+            Cyron__C4gv = Cyron__v876['410']
+        case 411:
+            Cyron__C4gv = 309
+        case 476:
+            say_hi(name)
+            Cyron__C4gv += 12
+        case 471:
+            Cyron__C4gv += 15
+        case 425:
+            Cyron__C4gv = 419
+        case 398:
+            Cyron__C4gv = Cyron__v876['400']
+        case _:
+            Cyron__v876[str(Cyron__C4gv)] = Cyron__C4gv + Cyron__dvkj
+            Cyron__C4gv -= Cyron__G554
